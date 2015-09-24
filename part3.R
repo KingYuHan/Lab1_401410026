3ecbc0af28cdb96dabe2ef4870b46ccee43afe36
KingYuHan <king810169@gmail.com>
2015-09-24 11:40:15 +0800

    part2.R part3.R

---
+++ part3.R
@@ -0,0 +1,24 @@
+wingcrd<-c(59,55,53.5,55,52.5,57.5,53,55)
+Tarsus<-c(22.3,19.7,20.8,20.3,20.8,21.5,20.6,21.5)
+Head<-c(31.2,30.4,30.6,30.3,30.3,30.8,32.5,NA)
+Wt<-c(9.5,13.8,14.8,15.2,15.5,15.6,15.6,15.7)
+mean(wingcrd)
+median(wingcrd)
+min(wingcrd)
+max(wingcrd)
+
+mean(Tarsus)
+median(Tarsus)
+min(Tarsus)
+max(Tarsus)
+
+mean(Head, na.rm = TRUE)
+median(Head, na.rm = TRUE)
+min(Head, na.rm = TRUE)
+max(Head, na.rm = TRUE)
+
+mean(Wt)
+median(Wt)
+min(Wt)
+max(Wt)
+
